d7b047f00a7848d0ee71f321d11d97199e8a0937
Chernikava, Alena <AlenaChernikava@Eaton.com>
2015-11-10 16:38:44 +0100

    Problem: threshold rules are not supported Solution: support them

--- src/alert_agent.cc
+++ src/alert_agent.cc
@@ -364,6 +364,147 @@ private:
     std::set<std::string> _in;
 
 };
+class ThresholdRule : public Rule
+{
+public:
+    ThresholdRule(){};
+
+    int evaluate (const MetricList &metricList, PureAlert **pureAlert) const
+    {
+        lua_State *lua_context = setContext (metricList);
+        if ( lua_context == NULL ) {
+            // not possible to evaluate metric with current known Metrics
+            return 2;
+        }
+
+        zsys_info ("lua_code = %s", _lua_code.c_str() );
+        int error = luaL_loadbuffer (lua_context, _lua_code.c_str(), _lua_code.length(), "line") ||
+            lua_pcall (lua_context, 0, 3, 0);
+
+        if ( error ) {
+            // syntax error in evaluate
+            zsys_info ("Syntax error: %s\n", lua_tostring(lua_context, -1));
+            lua_close (lua_context);
+            return 1;
+        }
+        // if we are going to use the same context repeatedly -> use lua_pop(lua_context, 1)
+        // to pop error message from the stack
+
+        // evaluation was successful, need to read the result
+        if ( !lua_isstring (lua_context, -1) ) {
+            zsys_info ("unexcpected returned value\n");
+            lua_close (lua_context);
+            return -1;
+        }
+        // ok, in the lua stack we got, what we expected
+        const char *status_ = lua_tostring(lua_context, -1); // IS / ISNT
+        zsys_info ("status = %s", status_ );
+        int s = ALERT_UNKNOWN;
+        if ( strcmp (status_, "IS") == 0 ) {
+            s = ALERT_START;
+        }
+        else if ( strcmp (status_, "ISNT") == 0 ) {
+            s = ALERT_RESOLVED;
+        }
+        if ( s == ALERT_UNKNOWN ) {
+            zsys_info ("unexcpected returned value, expected IS/ISNT\n");
+            lua_close (lua_context);
+            return -5;
+        }
+        if ( !lua_isstring(lua_context, -3) ) {
+            zsys_info ("unexcpected returned value\n");
+            lua_close (lua_context);
+            return -3;
+        }
+        const char *description = lua_tostring(lua_context, -3);
+        *pureAlert = new PureAlert(s, ::time(NULL), description, _element);
+        printPureAlert (**pureAlert);
+        lua_close (lua_context);
+        return 0;
+    };
+
+    bool isTopicInteresting(const std::string &topic) const
+    {
+        return ( _in == topic ? true : false );
+    };
+
+    std::set<std::string> getNeededTopics(void) const {
+        return {_in};
+    };
+
+    friend Rule* readRule (std::ifstream &f);
+
+protected:
+
+    void generateLua (void)
+    {
+        // assumption: at this point type can have only two values
+        if ( _type == "low" )
+        {
+            _lua_code = "if ( ";
+            _lua_code += _metric;
+            _lua_code += "_";
+            _lua_code += _element;
+            _lua_code += " <  ";
+            _lua_code += std::to_string(_value);
+            _lua_code += " ) then return \"Element ";
+            _lua_code += _element;
+            _lua_code += " is lower than threshold";
+            _lua_code += "\", ";
+            _lua_code += std::to_string(_value);
+            _lua_code += ", \"IS\" else return \"\", ";
+            _lua_code += std::to_string(_value);
+            _lua_code += " , \"ISNT\" end";
+        }
+        else
+        {
+            _lua_code = "if ( ";
+            _lua_code += _metric;
+            _lua_code += "_";
+            _lua_code += _element;
+            _lua_code += " >  ";
+            _lua_code += std::to_string(_value);
+            _lua_code += " ) then return \"Element ";
+            _lua_code += _element;
+            _lua_code += " is higher than threshold";
+            _lua_code += "\", ";
+            _lua_code += std::to_string(_value);
+            _lua_code += ", \"IS\" else return \"\", ";
+            _lua_code += std::to_string(_value);
+            _lua_code += " , \"ISNT\" end";
+        }
+        zsys_info ("generated_lua = %s", _lua_code.c_str());
+    };
+
+    void generateNeededTopic (void)
+    {
+        // it is bad to open the notion, how topic is formed, but for now there is now better place
+        _in = _metric + "@" + _element;
+    };
+
+    lua_State* setContext (const MetricList &metricList) const
+    {
+        lua_State *lua_context = lua_open();
+        double neededValue = metricList.find (_in);
+        if ( isnan (neededValue) ) {
+            zsys_info("Do not have everything for '%s' yet\n", _rule_name.c_str());
+            lua_close (lua_context);
+            return NULL;
+        }
+        std::string var = _metric + "_" + _element;
+        zsys_info("Setting variable '%s' to %lf\n", var.c_str(), neededValue);
+        lua_pushnumber (lua_context, neededValue);
+        lua_setglobal (lua_context, var.c_str());
+        return lua_context;
+    };
+
+private:
+    std::string _metric;
+    std::string _type;
+    double _value;
+    // this field is generated field
+    std::string _in;
+};
 
 class RegexRule : public Rule {
 public:
@@ -485,20 +626,32 @@ Rule* readRule (std::ifstream &f)
         si->getMember("evaluation") >>= rule->_lua_code;
         si->getMember("rule_name") >>= rule->_rule_name;
         si->getMember("severity") >>= rule->_severity;
+        zsys_info ("lua = %s", rule->_lua_code.c_str());
+        return rule;
+    }
+    else if ( si->findMember("in_rex") ) {
+        RegexRule *rule = new RegexRule();
+        si->getMember("in_rex") >>= rule->_rex_str;
+        rule->_rex = zrex_new(rule->_rex_str.c_str());
+        si->getMember("evaluation") >>= rule->_lua_code;
+        si->getMember("rule_name") >>= rule->_rule_name;
+        si->getMember("severity") >>= rule->_severity;
+        zsys_info ("lua = %s", rule->_lua_code.c_str());
+        return rule;
+    } else if ( si->findMember("metric") ){
+        ThresholdRule *rule = new ThresholdRule();
+        si->getMember("metric") >>= rule->_metric;
+        si->getMember("element") >>= rule->_element;
+        si->getMember("rule_name") >>= rule->_rule_name;
+        si->getMember("severity") >>= rule->_severity;
+        si->getMember("type") >>= rule->_type;
+        si->getMember("value") >>= rule->_value;
+        rule->generateLua();
+        rule->generateNeededTopic();
         return rule;
     }
     else {
-        if ( si->findMember("in_rex") ) {
-            RegexRule *rule = new RegexRule();
-            si->getMember("in_rex") >>= rule->_rex_str;
-            rule->_rex = zrex_new(rule->_rex_str.c_str());
-            si->getMember("evaluation") >>= rule->_lua_code;
-            si->getMember("rule_name") >>= rule->_rule_name;
-            si->getMember("severity") >>= rule->_severity;
-            return rule;
-        }
-        else
-            return NULL;
+        return NULL;
     }
 };
 
