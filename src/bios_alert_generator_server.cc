de95b1020e41fa59c5e5057eb7a396d7fc5d9c4a
Chernikava, Alena <AlenaChernikava@Eaton.com>
2016-04-19 16:36:25 +0200

    Problem: unused variable Solution: remove it

--- src/bios_alert_generator_server.cc
+++ src/bios_alert_generator_server.cc
@@ -641,7 +641,7 @@ bios_alert_generator_server (zsock_t *pipe, void* args)
 
                 // Read initial configuration
                 alertConfiguration.setPath(filename);
-                std::set <std::string> subjectsToConsume = alertConfiguration.readConfiguration();
+                alertConfiguration.readConfiguration();
 
                 zstr_free (&filename);
             }
@@ -672,7 +672,7 @@ bios_alert_generator_server (zsock_t *pipe, void* args)
                 zsys_debug1 ("cannot decode message, ignoring\n");
                 continue;
             }
-            if ( bios_proto_id(bmessage) == BIOS_PROTO_METRIC )  {                
+            if ( bios_proto_id(bmessage) == BIOS_PROTO_METRIC )  {
                 // process as metric message
                 const char *type = bios_proto_type(bmessage);
                 const char *element_src = bios_proto_element_src(bmessage);
