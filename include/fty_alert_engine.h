720741f01d3de4bf2649ed98f6463cf2934bb8a7
Tomas Halman <TomasHalman@eaton.com>
2016-12-20 10:30:50 +0100

    Problem: I want to rename bios classes to fty classes
    Solution: fix project.xml, regenerate, fix compilation issues
    
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

---
+++ include/fty_alert_engine.h
@@ -0,0 +1,30 @@
+/*  =========================================================================
+    fty-alert-engine - 42ity service evaluating rules written in Lua and producing alerts
+
+    Copyright (C) 2014 - 2015 Eaton                                        
+                                                                           
+    This program is free software; you can redistribute it and/or modify   
+    it under the terms of the GNU General Public License as published by   
+    the Free Software Foundation; either version 2 of the License, or      
+    (at your option) any later version.                                    
+                                                                           
+    This program is distributed in the hope that it will be useful,        
+    but WITHOUT ANY WARRANTY; without even the implied warranty of         
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+    GNU General Public License for more details.                           
+                                                                           
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    =========================================================================
+*/
+
+#ifndef FTY_ALERT_ENGINE_H_H_INCLUDED
+#define FTY_ALERT_ENGINE_H_H_INCLUDED
+
+//  Include the project library file
+#include "fty_alert_engine_library.h"
+
+//  Add your own public definitions here, if you need them
+
+#endif
