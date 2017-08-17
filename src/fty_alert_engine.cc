3918b23d6fc5d0da24c0a0d664e4c8e46f4746f2
bstepankova <bstepankova@volny.cz>
2017-08-17 14:05:56 +0200

    Problem: incorrect actor choosen
    Solution: corrected
    
    Signed-off-by: Barbora Stepankova <BarboraStepankova@eaton.com>

--- src/fty_alert_engine.cc
+++ src/fty_alert_engine.cc
@@ -54,7 +54,6 @@ int main (int argc, char** argv)
     zstr_sendx (ag_server, "CONFIG", PATH, NULL);
     zstr_sendx (ag_server, "PRODUCER", FTY_PROTO_STREAM_ALERTS_SYS, NULL);
     zstr_sendx (ag_server, "CONSUMER", FTY_PROTO_STREAM_METRICS, ".*", NULL);
-    zstr_sendx (ag_server, "CONSUMER", FTY_PROTO_STREAM_METRICS_SENSOR, ".*", NULL);
     zstr_sendx (ag_server, "CONSUMER", FTY_PROTO_STREAM_METRICS_UNAVAILABLE, ".*", NULL);
 
     zactor_t *ag_configurator = zactor_new (autoconfig, (void*) AUTOCONFIG_NAME);
@@ -63,6 +62,7 @@ int main (int argc, char** argv)
         zstr_sendx (ag_configurator, "VERBOSE", NULL);
     zstr_sendx (ag_configurator, "CONNECT", ENDPOINT, NULL);
     zstr_sendx (ag_configurator, "TEMPLATES_DIR", "/usr/share/bios/fty-autoconfig", NULL);
+    zstr_sendx (ag_server, "CONSUMER", FTY_PROTO_STREAM_METRICS_SENSOR, ".*", NULL);
     zstr_sendx (ag_configurator, "CONSUMER", FTY_PROTO_STREAM_ASSETS, ".*", NULL);
     zstr_sendx (ag_configurator, "ALERT_ENGINE_NAME", AGENT_NAME, NULL);
     //  Accept and print any message back from server
