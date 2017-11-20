ce025917c0beba7423c491f4fde8eb28aafce638
bstepankova <bstepankova@volny.cz>
2017-11-20 16:48:34 +0100

    Problem: iname is displayed in alarm message instead if ename
    Solution: fixed
    
    Signed-off-by: Barbora Stepankova <BarboraStepankova@eaton.com>

--- src/ruleconfigurator.h
+++ src/ruleconfigurator.h
@@ -1,21 +1,21 @@
 /*  =========================================================================
     ruleconfigurator - Rule Configurator
 
-    Copyright (C) 2014 - 2017 Eaton                                        
-                                                                           
-    This program is free software; you can redistribute it and/or modify   
-    it under the terms of the GNU General Public License as published by   
-    the Free Software Foundation; either version 2 of the License, or      
-    (at your option) any later version.                                    
-                                                                           
-    This program is distributed in the hope that it will be useful,        
-    but WITHOUT ANY WARRANTY; without even the implied warranty of         
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
-    GNU General Public License for more details.                           
-                                                                           
+    Copyright (C) 2014 - 2017 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     =========================================================================
 */
 
@@ -38,20 +38,20 @@ extern "C" {
 
 
 class RuleConfigurator {
-  public:  
-    virtual bool configure (const std::string& name, const AutoConfigurationInfo& info)
+  public:
+    virtual bool configure (const std::string& name, const AutoConfigurationInfo& info, const std::string &logical_asset)
     {
-        return configure (name, info, NULL);
+        return configure (name, info, logical_asset, NULL);
     }
 
-    virtual bool configure (const std::string& name, const AutoConfigurationInfo& info, mlm_client_t *client)
-    { 
+    virtual bool configure (const std::string& name, const AutoConfigurationInfo& info, const std::string &logical_asset, mlm_client_t *client)
+    {
         return false;
     }
     virtual bool isApplicable (UNUSED_PARAM const AutoConfigurationInfo& info)
-    {   
+    {
         return false;
-    }   
+    }
 
     bool sendNewRule (const std::string& rule, mlm_client_t *client);
 
@@ -72,10 +72,10 @@ class RuleConfigurator {
           const std::string& element_name,
           //                          value_name   value
           const std::vector <std::pair<std::string, std::string>>& values,
-          //                           result_name               actions       severity     description 
+          //                           result_name               actions       severity     description
           const std::vector <std::tuple<std::string, std::vector <std::string>, std::string, std::string>>& results,
           const std::string& evaluation);
-                                              
+
     std::string makeSingleRule_results (std::tuple<std::string, std::vector <std::string>, std::string, std::string> result);
 
   // TODO:
@@ -84,7 +84,7 @@ class RuleConfigurator {
   //      makePatternRule
 
     virtual ~RuleConfigurator() {};
-    
+
 };
 
 #ifdef __cplusplus
