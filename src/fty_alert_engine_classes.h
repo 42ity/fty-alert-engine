b85d2d96f4dbeae9b1feb20656b26ffbb4265802
Jim Klimov <jim@jimklimov.com>
2019-10-11 13:58:26 +0200

    Bump copyright of ecosystem to 2014-2019

--- src/fty_alert_engine_classes.h
+++ src/fty_alert_engine_classes.h
@@ -1,7 +1,7 @@
 /*  =========================================================================
     fty_alert_engine_classes - private header file
 
-    Copyright (C) 2015 - 2019 Eaton
+    Copyright (C) 2014 - 2019 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
