0f8759dd64182980d64d9c80327619f7014fb445
Degott, Francois Regis <FrancoisRegisDegott@eaton.com>
2021-07-29 17:48:54 +0200

    fix/improve audit-test
    Signed-off-by: Degott, Francois Regis <FrancoisRegisDegott@eaton.com>

--- test/audit_test.cpp
+++ test/audit_test.cpp
@@ -4,35 +4,53 @@
 
 #include <sys/stat.h>
 #include <unistd.h>
+#include <iostream>
 #include <fstream>
+#include <filesystem>
 
-TEST_CASE("audit test")
+TEST_CASE("audit-test")
 {
     const bool verbose = false;
+    std::cout << "-- udit-test --" << std::endl;
+
+    {
+        using std::filesystem::current_path;
+        char tmp[256];
+        getcwd(tmp, 256);
+        std::cout << "Current working directory: " << tmp << std::endl;
+    }
 
     ManageFtyLog::setInstanceFtylog ("fty-alert-engine-audit-test");
     if (verbose)
         ManageFtyLog::getInstanceFtylog()->setVeboseMode();
 
+    std::string LOG_CONFIG_FILE = "./test/audit/fty-alert-engine-log-test.cfg";
+
     // load log config file (MaxFileSize=1MB, MaxBackupIndex=3)
-    std::string logConfigFile = "./fty-alert-engine-log-test.cfg";
-    ManageFtyLog::getInstanceFtylog()->setConfigFile(logConfigFile);
+    std::cout << "Loading " << LOG_CONFIG_FILE << std::endl;
+    ManageFtyLog::getInstanceFtylog()->setConfigFile(LOG_CONFIG_FILE);
 
     // initialize log for auditability
-    AlertsEngineAuditLogManager::init(logConfigFile.c_str());
+    std::cout << "Audit init. " << LOG_CONFIG_FILE << std::endl;
+    AlertsEngineAuditLogManager::init(LOG_CONFIG_FILE.c_str());
 
     // fulfill logs
+    std::cout << "Fulfill logs" << std::endl;
     const int NB_LOG = 100000;
     for (int i=0; i < NB_LOG; i++) {
       log_info_alarms_engine_audit("AUDIT LOG TEST %0.5d", i);
     }
 
+    std::string LOG_OUTPUT_FILE = "/tmp/alarms-audit-test.log";
+
     // check if file log  is created
-    assert(access("./alarms-audit-test.log", F_OK) != -1);
+    std::cout << "Check logs access" << std::endl;
+    CHECK(access(LOG_OUTPUT_FILE.c_str(), F_OK) != -1);
 
     // check if file log size is superior to 0
+    std::cout << "Check logs size" << std::endl;
     {
-        std::ifstream file("./alarms-audit-test.log", std::ifstream::in | std::ifstream::binary);
+        std::ifstream file(LOG_OUTPUT_FILE, std::ifstream::in | std::ifstream::binary);
         file.seekg(0, std::ios::end);
         auto fileSize = file.tellg();
         CHECK(fileSize > 0);
@@ -41,8 +59,8 @@ TEST_CASE("audit test")
     // for each archive file (NB=3)
     for (char c= '1'; c <= '3'; c++) {
         // check if archive file is created
-        std::string log_file = "./alarms-audit-test.log.";
-        log_file += c;
+        std::string log_file = LOG_OUTPUT_FILE + "." + c;
+        std::cout << "Check logs file:" << log_file << std::endl;
 
         // check if archive file is created
         CHECK(access(log_file.c_str(), F_OK) != -1);
@@ -56,8 +74,9 @@ TEST_CASE("audit test")
         //delete the archive file test
         remove(log_file.c_str());
     }
-    //delete the log file test
-    remove("./alarms-audit-test.log");
+
+    // delete the log file test
+    remove(LOG_OUTPUT_FILE.c_str());
 
     // release audit context
     AlertsEngineAuditLogManager::deinit();
