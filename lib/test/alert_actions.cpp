0dfb06e04e7ceb4c503e7820b0715a46fc2785a0
Francois-Regis Degott <FrancoisRegisDegott@eaton.com>
2025-06-15 12:12:18 +0000

    lib/test/ : misc
    Signed-off-by: Francois-Regis Degott <FrancoisRegisDegott@eaton.com>

--- lib/test/alert_actions.cpp
+++ lib/test/alert_actions.cpp
@@ -1,8 +1,27 @@
-#include "src/fty_alert_actions.h"
+/*
+Copyright (C) 2014 - 2020 Eaton
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include <catch2/catch.hpp>
+
+#include "src/fty_alert_actions.h"
 #include <fty_log.h>
 
-TEST_CASE("alert actions test - static")
+TEST_CASE("alert actions - static")
 {
     setenv("BIOS_LOG_PATTERN", "%D %c [%t] -%-5p- %M (%l) %m%n", 1);
     ManageFtyLog::setInstanceFtylog("fty-alert-actions");
@@ -237,7 +256,7 @@ TEST_CASE("alert actions test - static")
     }
 }
 
-TEST_CASE("alert actions test - mlm")
+TEST_CASE("alert actions - malamute")
 {
     setenv("BIOS_LOG_PATTERN", "%D %c [%t] -%-5p- %M (%l) %m%n", 1);
     ManageFtyLog::setInstanceFtylog("fty-alert-actions");
--- lib/test/alertconfiguration.cpp
+++ lib/test/alertconfiguration.cpp
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2014 - 2020 Eaton
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include <catch2/catch.hpp>
 
 #include "src/rule/rule.h"
@@ -55,7 +73,7 @@ TEST_CASE("rule outcome tokens")
     }
 }
 
-TEST_CASE("alertconfiguration test")
+TEST_CASE("alertconfiguration")
 {
     setenv("BIOS_LOG_PATTERN", "%D %c [%t] -%-5p- %M (%l) %m%n", 1);
     ManageFtyLog::setInstanceFtylog("fty-alert-configuration");
